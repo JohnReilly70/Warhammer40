9e42c50e887df9402637171405698c6ee0eea637
SgtSafety <johnreilly1989@hotmail.co.uk>
2016-10-16 15:55:00 +0100

    Added Add_And_Remove_Armour Function / Need to complete the addition of Armour EXTRA info

--- Character_Classes.py
+++ Character_Classes.py
@@ -2,7 +2,6 @@
 class Character:
     '''
     Base Character Class which creates all Players & NPCs
-
     '''
 
     def __init__(self, name):
@@ -15,7 +14,8 @@ class Character:
         self.renown = 0
         self.talents_traits = dict() # empty dicts equivalent too '== {}'
         self.psychic = dict()
-        self.armour = {'Head' : 0, 'Chest' : 0, 'L Arm' : 0, 'R Arm' : 0, 'L Leg' : 0, 'R Leg' : 0}
+        # Ratings Values going left to right should be 'Head' : 0, 'Chest' : 0, 'L Arm' : 0, 'R Arm' : 0, 'L Leg' : 0, 'R Leg' : 0
+        self.armour = {'Rating' : [], 'Info' : {'Name' : '', 'Weight' : 0, 'Req' : 0, 'Renown' : 0}}
         self.weapons = {'Melee' : {}, 'Ranged' : {}}
 
     def Add_Weapon(self, *args):
@@ -32,13 +32,33 @@ class Character:
             print("\nFailed to Enter Correct Weapon Data\n\nWords in same column should be kept together ie \"Tearing, Balanced\"")
 
     def Remove_Weapon(self, Weapon_Name):
-        # Find solution that checks if either Dict has the name in it and only prints out KeyError if not in both
-        try:
+        '''
+        Check If the weapon they have named is in either Melee or Ranged Dicts of the weapon Dict
+        Else Print the name does not have that weapon
+        '''
+        if Weapon_Name in self.weapons['Melee']:
             del self.weapons['Melee'][Weapon_Name]
+        elif Weapon_Name in self.weapons['Ranged']:
             del self.weapons['Ranged'][Weapon_Name]
-        except KeyError:
+        else:
             print("\n{} does not have that in his armoury.".format(self.name))
 
-    def Show_Inventory(self):
-                print("{}\n".format(self.weapons))
+    def Show_Inventory(self, Inv_Type=''):
+        '''
+        Shows All Inventory if Method receives no args or incorrectly spelled arg
+        '''
+        if Inv_Type == 'Melee':
+            print("{}\n".format(self.weapons['Melee']))
+            return True
+        elif Inv_Type == 'Ranged':
+            print("{}\n".format(self.weapons['Ranged']))
+            return True
+        else:
+            print("{}\n".format(self.weapons))
+            return True
+        # If this IF statement fails it should return False. Should NEVER return False
+        return False
 
+    def Add_And_Replace_Armour(self, Name, Armour_Rating_List):
+        self.armour['Info']['Name'] = Name
+        self.armour['Rating'] = Armour_Rating_List
--- Unit_Test_Area.py
+++ Unit_Test_Area.py
@@ -22,6 +22,7 @@ class Test_Class_Creation(unittest.TestCase):
     def test_Preset_Stats(self):
         # Check Stats are all set up at minimum value
         player = Character_Classes.Character("John")
+
         self.assertEqual(player.rank, 0)
         self.assertEqual(player.wounds, 1)
         self.assertEqual(player.fate, 0)
@@ -33,16 +34,15 @@ class Test_Class_Creation(unittest.TestCase):
     def test_Armour_Dict(self):
         # Check Armour Values are a Dict with 0 as initial
         player = Character_Classes.Character("John")
-        self.assertEqual(player.armour['Head'], 0)
-        self.assertEqual(player.armour['Chest'], 0)
-        self.assertEqual(player.armour['L Arm'], 0)
-        self.assertEqual(player.armour['R Arm'], 0)
-        self.assertEqual(player.armour['L Leg'], 0)
-        self.assertEqual(player.armour['R Leg'], 0)
+
+        for index, Armour_Piece in enumerate(player.armour['Rating']):
+            self.assertEqual(player.armour['Rating'][index], 0)
+
 
     def test_Weapon_Dict(self):
         # Check an empty Dict is started for Weapon
         player = Character_Classes.Character("John")
+
         self.assertFalse(player.weapons['Melee'])
         self.assertFalse(player.weapons['Ranged'])
 
@@ -51,34 +51,53 @@ class Test_Class_Creation(unittest.TestCase):
         # Weapons SHould Have Name/Class/Dmg/Pen/Special/Weight/Requistion/MinRenown
         player.Add_Weapon("Astartes Chainsword", "Melee", "1d10+3 R", 4, "Balanced, Tearing", 10, 5, 0)
         player.Add_Weapon("Astartes ChainFist", "Melee", "3d10+3 R", 7, "Tearing", 10, 5, 2)
+
         self.assertEqual(player.weapons['Melee']['Astartes Chainsword'], ['Melee', '1d10+3 R', 4, 'Balanced, Tearing', 10, 5, 0])
         self.assertEqual(player.weapons['Melee']['Astartes ChainFist'], ["Melee", "3d10+3 R", 7, "Tearing", 10, 5, 2])
 
     def test_Add_Weapon_Insufficent_Info(self):
         # Will not add a weapon if all the info is not there. (Min Info as per Table 5-5 / 5-8 DeathWatch Core RuleBook)
         player = Character_Classes.Character("John")
+
         self.assertFalse(player.Add_Weapon("Astartes Bolter", "Melee", "3d10+3 R", 7, "Tearing"))
 
     def test_Remove_Weapon(self):
         # Removes Weapon if Weapon name is correct (Only info required to remove a weapon)
         player = Character_Classes.Character("John")
         player.Add_Weapon("Astartes Chainsword", "Melee", "1d10+3 R", 4, "Balanced, Tearing", 10, 5, 0)
-        print(player.weapons)
         player.Remove_Weapon("Astartes Chainsword")
-        print(player.weapons)
+
         self.assertFalse(player.weapons['Melee'])
 
     def test_Remove_Weapon_Not_In_Inventory(self):
         # Returns a message stating that weapon name is not in inventory (self.weapons)
         player = Character_Classes.Character("John")
         player.Remove_Weapon("Astartes Chainsword")
+
         self.assertFalse(player.weapons['Melee'])
 
     def test_Show_Inventory(self):
         player = Character_Classes.Character("John")
         player.Add_Weapon("Astartes Chainsword", "Melee", "1d10+3 R", 4, "Balanced, Tearing", 10, 5, 0)
         player.Add_Weapon("Astartes Bullet", "Bullet", "1d10+3 R", 4, "Balanced, Tearing", 10, 5, 0)
-        player.Show_Inventory()
+
+        print("All Inventory:\n")
+        self.assertTrue(player.Show_Inventory())
+        print("Melee Inventory:\n")
+        self.assertTrue(player.Show_Inventory("Melee"))
+        print("Ranged Inventory:\n")
+        self.assertTrue(player.Show_Inventory("Ranged"))
+
+    def test_Add_Armour_without_Addition_Info(self):
+        player = Character_Classes.Character("John")
+        player.Add_And_Replace_Armour("Carapace Armour", [8, 8, 8, 8, 8, 8])
+
+        self.assertEqual(player.armour['Info']['Name'], 'Carapace Armour')
+        for index, Armour_Piece in enumerate(player.armour['Rating']):
+            self.assertEqual(player.armour['Rating'][index], 8)
+
+
+
 
 
 
